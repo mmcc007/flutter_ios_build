22a7488c8cfb01289fcd73e2a82a9eb20d3cc4ad
Maurice McCabe <mmcc007@gmail.com>
2019-06-12 23:43:30 -0700

    Added methods to (re)install and re-sign from .app and .ipa artifacts

--- script/ios_device_test.sh
+++ script/ios_device_test.sh
@@ -14,51 +14,62 @@ testable_ipa='Debug_Runner.ipa'
 
 main(){
   # if no arguments passed
-  if [[ "$#" -eq 0 ]]; then show_help; fi
+  if [[ "$#" -eq 0 ]]; then show_usage; exit 1; fi
 
   case $1 in
       --download)
-          if [[ "$#" -ne 2 ]]; then show_help; fi
-          download_test_artifacts $2
+          if [[ "$#" -ne 2 ]]; then show_usage; exit 1; fi
+          download_project_artifacts $2
+          ;;
+      --install_app)
+          install_app
+          ;;
+      --install_ipa)
+          install_ipa
           ;;
       --resign)
-          if [[ "$#" -ne 4 ]]; then show_help; fi
-          re-sign "${2}" "${4}"
+          if [[ "$#" -ne 5 ]]; then show_usage; exit 1; fi
+          re-sign $2 "${3}" "${5}"
           ;;
       --test)
           run_test_flutter_no_build
           ;;
-      --unpack)
-          unpack_testable_app
+      --install)
+          # for dev testing
+          install_testable_ipa 'build/ios/Debug-iphoneos/Runner.ipa'
           ;;
       *)
-          show_help
+          show_usage
+          exit 1
           ;;
   esac
 }
 
-show_help() {
+show_usage() {
     local script_name=$(basename "$0")
-    printf "\nusage: $script_name [--download <release tag>] [--unpack] [--resign <cert name> <provisioning path>] [--test]
+    printf "\nusage: $script_name [--download <release tag>] [--install_app] [--install_ipa] [--resign <mode> <cert name> <provisioning path>] [--test]
 where:
     --download
-        downloads the src (with test), signed testable .app and signed non-testable .ipa.
+        downloads the src (with test), signed testable .app, signed testable .ipa,
+        and signed non-testable .ipa.
         release tag can be found at $project_artifacts_base/$app_name/releases/latest
-    --unpack
-        unpack and install the testable .ipa
+    --install_app
+        (re)install testable .app
+    --install_ipa
+        (re)install testable .app from testable .ipa
     --resign
-        re-signs the testable .app using local developer account
+        re-sign and (re)install the testable .app or testable .ipa using local developer account.
+        mode is 'app' or 'ipa'
     --test
-        runs the integration test
+        runs the integration test using the currently installed testable .app
 
 Sample usage:
-$script_name --resign 'iPhone Distribution: Maurice McCabe (ABCDEFGHIJ)' /Users/jenkins/Library/MobileDevice/Provisioning\ Profiles/408fa202-3212-469d-916c-c7f2ae4d083a.mobileprovision
+$script_name --resign ipa 'iPhone Distribution: Maurice McCabe (ABCDEFGHIJ)' /Users/jenkins/Library/MobileDevice/Provisioning\ Profiles/408fa202-3212-469d-916c-c7f2ae4d083a.mobileprovision
 "
-    exit 1
 }
 
 # download project with signed testable .app and non-testable .ipa
-download_test_artifacts(){
+download_project_artifacts(){
   local release_tag=$1
 
   local non_testable_ipa='Runner.ipa'
@@ -76,9 +87,9 @@ download_test_artifacts(){
   wget $app_src_url
   unzip "$release_tag.zip"
 
-  # download and setup testable .app
+  # download and install testable .app in build directory
   cd "$app_name-$release_tag"
-  wget $testable_app_url
+  wget -O "$testable_app.zip" $testable_app_url
   unzip "$testable_app.zip"
   mkdir $build_dir
   # keep a copy of testable .app for repeated re-signing
@@ -91,24 +102,50 @@ download_test_artifacts(){
   wget $non_testable_ipa_url
 }
 
-# re-sign signed testable .app with local developer account
+# install or re-install from testable .app artifact to build directory
+install_app(){
+  cd $(find_app_dir)
+  rm -rf "$debug_build_dir/$testable_app"
+  unzip "$testable_app.zip"
+
+  # copy to build dir for testing
+  rm -rf "$build_dir/$testable_app"
+  cp -r "$debug_build_dir/$testable_app" $build_dir
+}
+
+# install or re-install from testable .ipa artifact to build directory
+install_ipa(){
+  cd $(find_app_dir)
+  install_testable_ipa 'Runner.ipa'
+}
+
+# re-sign testable .app or testable .ipa with local apple developer account
 re-sign(){
-  local cert_name=$1
-  local provisioning_profile_path=$2
+  local resign_mode=$1
+  local cert_name=$2
+  local provisioning_profile_path=$3
   local resigned_app_dir='/tmp/resigned'
 
   cd $(find_app_dir)
 
-  # resign testable .app
+  # resign testable .app or testable .ipa
+  local input_file=''
+  local output_file=''
+  if [[ "$resign_mode" == 'app' ]]; then
+    input_file="$debug_build_dir/$testable_app"
+    output_file="$resigned_app_dir/$testable_app"
+  else
+    input_file="$testable_ipa"
+    output_file="$resigned_app_dir/$testable_ipa"
+  fi
   rm -rf $resigned_app_dir
   mkdir $resigned_app_dir
-  ./script/resign.sh "$debug_build_dir/$testable_app" "$cert_name" --provisioning "$provisioning_profile_path" --verbose "$resigned_app_dir/$testable_app"
+  ./script/resign.sh "$input_file" "$cert_name" --provisioning "$provisioning_profile_path" --verbose "$output_file"
 
   # over-write original testable .app with re-signed testable .app
   rm -rf "$build_dir/$testable_app"
-  unzip $resigned_app_dir/$testable_app -d $resigned_app_dir
+  unzip $output_file -d $resigned_app_dir
   mv $resigned_app_dir/Payload/$testable_app $build_dir
-
 }
 
 # run test without flutter tools
@@ -193,8 +230,8 @@ find_app_dir(){
 }
 
 # unpack testable .app from .ipa and install
-unpack_testable_app(){
-  local testable_ipa_path='build/ios/Debug-iphoneos/Runner.ipa'
+install_testable_ipa(){
+  local testable_ipa_path=$1
   local unpack_dir='/tmp/unpack_testable_ipa'
 
   # clear unpack directory
@@ -203,7 +240,7 @@ unpack_testable_app(){
 
   unzip $testable_ipa_path -d $unpack_dir
 
-  # move to build area
+  # move to build area for testing
   rm -rf "$build_dir/$testable_app"
   mv $unpack_dir/Payload/$testable_app $build_dir
 
